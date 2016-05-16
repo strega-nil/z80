2308b5f0dbcceb3b3a72e44600fe2a16e664206a
ubsan <npmazzuca@gmail.com>
2016-05-16 10:13:20 -0700

    Clippy, plus a few extra things

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 // TODO(ubsan): p flag
 // TODO(ubsan): h flag
 // TODO(ubsan): n flag
-
 #![feature(box_syntax, question_mark)]
+
+//use std::io::BufRead;
+use std::io::Read;
+
 fn main() {
-  use std::io::Read;
   let mut state = State::with_memory(&[
-      0x0C, // inc b
-      0x0D, // dec b
-      0x28, 0xFF,
+      0x06, 0x05,       // LD B, 05
+      0x3E, 0x00,       // LD A, 00
+      0x16, 0x01,       // LD D, 01
+      0x26, 0x00,       // LD H, 00
+      // loop:
+      0x82,             // ADD A, D
+      0x67,             // LD H, A
+      0xEB,             // EX DE, HL
+      0x10, 0xFD,       // DJNZ loop
+
+      0x76,             // HALT
   ]);
+
   println!("{:?}", state);
-  for _ in std::io::stdin().bytes() {
-    if state.memory[state.pc as usize] == 0 { break; }
-    state.step();
+  'dbg: while state.step() {
+    'input: for c in std::io::stdin().bytes() {
+      match c.unwrap() {
+        b'q' => break 'dbg,
+        b'\n' | b'n' => break 'input,
+        _ => {
+          println!("?");
+          break 'input;
+        }
+      }
+    }
     println!("{:?}", state);
   }
 }
@@ -136,30 +155,32 @@ pub struct State {
 
 impl Debug for State {
   fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
-    writeln!(f, "A  CZPSNH  BC   DE   HL  A' CZPSNH' BC'  DE'  HL' v(PC)")?;
-    writeln!(f, "{:02X} {}{}{}{}{}{} {:04X} {:04X} {:04X} {:02X} {}{}{}{}{}{} \
-        {:04X} {:04X} {:04X} {:02X}{:02X}{:02X}",
-        // normal regs
+    writeln!(f, "A  CZPSNH  BC   DE   HL   PC  v(PC)")?;
+    writeln!(f, "{:02X} {}{}{}{}{}{} {:04X} {:04X} {:04X} \
+        {:04X} {:02X}{:02X}{:02X}",
         self.r.a,
-          // flags
         self.r.flags.c() as u8, self.r.flags.z() as u8,
         0/*self.r.flags.p() as u8*/, self.r.flags.s() as u8,
         0/*self.r.flags.n() as u8*/, 0/*self.flags.h() as u8*/,
-          // 16-bit
         self.r.bc(), self.r.de(), self.r.hl(),
 
-        // prime regs
+        self.pc,
+        self.memory[self.pc as usize], self.memory[(self.pc + 1) as usize],
+        self.memory[(self.pc + 2) as usize])?;
+
+    // prime regs
+    writeln!(f, "A' CZPSNH' BC'  DE'  HL'  SP  v(SP)")?;
+    writeln!(f, "{:02X} {}{}{}{}{}{} {:04X} {:04X} {:04X} \
+        {:04X} {:02X}{:02X}{:02X}",
         self.rp.a,
-          // flags
         self.rp.flags.c() as u8, self.rp.flags.z() as u8,
         0/*self.rp.flags.p() as u8*/, self.rp.flags.s() as u8,
         0/*self.rp.flags.n() as u8*/, 0/*self.rp.flags.h()*/,
-          // 16-bit
         self.rp.bc(), self.rp.de(), self.rp.hl(),
 
-        // (pc)
-        self.memory[self.pc as usize], self.memory[(self.pc + 1) as usize],
-        self.memory[(self.pc + 2) as usize])?;
+        self.sp,
+        self.memory[self.sp as usize], self.memory[(self.sp + 1) as usize],
+        self.memory[(self.sp + 2) as usize])?;
     Ok(())
   }
 }
@@ -260,7 +281,7 @@ impl State {
     match f {
       Flag::Djnz => {
         self.r.b -= 1;
-        self.r.b == 0
+        self.r.b != 0
       }
       Flag::Uncond => true,
       Flag::Nonzero => !self.r.flags.z(),
@@ -297,6 +318,45 @@ impl State {
     let tmp = self.get16(into) as u32 + self.get16(from) as u32;
     self.r.flags.set_c(tmp > 0xFFFF);
     self.set16(into, tmp as u16);
+  }
+
+  pub fn add8(&mut self, from: Op8) {
+    let tmp = self.r.a as u16 + self.get8(from) as u16;
+    self.r.flags.set_c(tmp > 0xFF);
+    let tmp = tmp as u8;
+    self.r.flags.set_z(tmp == 0);
+    self.r.flags.set_s((tmp as i8) < 0);
+
+    self.r.a = tmp;
+  }
+  pub fn adc8(&mut self, from: Op8) {
+    let tmp = self.r.a as u16 + self.get8(from) as u16
+        + self.r.flags.c() as u16;
+    self.r.flags.set_c(tmp > 0xFF);
+    let tmp = tmp as u8;
+    self.r.flags.set_z(tmp == 0);
+    self.r.flags.set_s((tmp as i8) < 0);
+
+    self.r.a = tmp;
+  }
+  pub fn sub8(&mut self, from: Op8) {
+    let tmp = self.r.a as i16 - self.get8(from) as i16;
+    self.r.flags.set_c(tmp < 0);
+    let tmp = tmp as u8;
+    self.r.flags.set_z(tmp == 0);
+    self.r.flags.set_s((tmp as i8) < 0);
+
+    self.r.a = tmp;
+  }
+  pub fn sbc8(&mut self, from: Op8) {
+    let tmp = self.r.a as i16 - self.get8(from) as i16
+        - self.r.flags.c() as i16;
+    self.r.flags.set_c(tmp < 0);
+    let tmp = tmp as u8;
+    self.r.flags.set_z(tmp == 0);
+    self.r.flags.set_s((tmp as i8) < 0);
+
+    self.r.a = tmp;
   }
 
   pub fn not(&mut self, arg: Op8) {
@@ -369,11 +429,20 @@ impl State {
     std::mem::swap(&mut self.r.a, &mut self.rp.a);
     std::mem::swap(&mut self.r.flags, &mut self.rp.flags);
   }
+
+  pub fn ex_de_hl(&mut self) {
+    std::mem::swap(&mut self.r.d, &mut self.r.h);
+    std::mem::swap(&mut self.r.e, &mut self.r.l);
+  }
+
+  pub fn halt(&mut self) {}
 }
 
 // at the end because lots of lines
 impl State {
-  pub fn step(&mut self) {
+  // returns true if the machine should continue
+  // returns false if it shut off
+  pub fn step(&mut self) -> bool {
     let opcode = self.get_imm8();
     match opcode {
       0x00 => {} // NOP
@@ -407,7 +476,7 @@ impl State {
       0x1B => self.dec16(Op16::DE),             // DEC DE
       0x1C => self.inc8(Op8::E),                // INC E
       0x1D => self.dec8(Op8::E),                // DEC E
-      0x1E => self.ld8(Op8::C, Op8::Imm),       // LD C, N
+      0x1E => self.ld8(Op8::E, Op8::Imm),       // LD E, N
       0x1F => self.rr(Op8::A),                  // RRA
 
       0x20 => self.jr(Flag::Nonzero),           // JRNZ NN
@@ -416,7 +485,7 @@ impl State {
       0x23 => self.inc16(Op16::HL),             // INC HL
       0x24 => self.inc8(Op8::H),                // INC H
       0x25 => self.dec8(Op8::H),                // DEC H
-      0x26 => self.ld8(Op8::HLd, Op8::Imm),     // LD (HL), N
+      0x26 => self.ld8(Op8::H, Op8::Imm),       // LD H, N
       0x27 => self.unimplemented_inst(),        // DAA
       0x28 => self.jr(Flag::Zero),              // JRZ N
       0x29 => self.add16(Op16::HL, Op16::HL),   // ADD HL, HL
@@ -436,114 +505,119 @@ impl State {
       0x36 => self.ld8(Op8::HLd, Op8::Imm),     // LD (HL), N
       0x37 => self.r.flags.set_c(true),         // SCF
       0x38 => self.jr(Flag::Carry),             // JRC N
-      0x39 => self.add16(Op16::HL, Op16::HL),   // ADD HL, HL
+      0x39 => self.add16(Op16::HL, Op16::SP),   // ADD HL, SP
       0x3A => self.add16(Op16::HL, Op16::Immd), // ADD HL, (NN)
-      0x3B => self.dec16(Op16::HL),             // DEC HL
-      0x3C => self.inc8(Op8::L),
-      0x3D => self.dec8(Op8::L),
-      0x3E => self.ld8(Op8::L, Op8::Imm),
-      0x3F => {
+      0x3B => self.dec16(Op16::SP),             // DEC SP
+      0x3C => self.inc8(Op8::A),                // INC A
+      0x3D => self.dec8(Op8::A),                // DEC A
+      0x3E => self.ld8(Op8::A, Op8::Imm),       // LD A, N
+      0x3F => {                                 // CCF
         let c = self.r.flags.c();
         self.r.flags.set_c(!c);
       },
 
-      0x40 => self.unimplemented_inst(),
-      0x41 => self.unimplemented_inst(),
-      0x42 => self.unimplemented_inst(),
-      0x43 => self.unimplemented_inst(),
-      0x44 => self.unimplemented_inst(),
-      0x45 => self.unimplemented_inst(),
-      0x46 => self.unimplemented_inst(),
-      0x47 => self.unimplemented_inst(),
-      0x48 => self.unimplemented_inst(),
-      0x49 => self.unimplemented_inst(),
-      0x4A => self.unimplemented_inst(),
-      0x4B => self.unimplemented_inst(),
-      0x4C => self.unimplemented_inst(),
-      0x4D => self.unimplemented_inst(),
-      0x4E => self.unimplemented_inst(),
-      0x4F => self.unimplemented_inst(),
+      0x40 => self.ld8(Op8::B, Op8::B),         // LD B, B
+      0x41 => self.ld8(Op8::B, Op8::C),         // LD B, C
+      0x42 => self.ld8(Op8::B, Op8::D),         // LD B, D
+      0x43 => self.ld8(Op8::B, Op8::E),         // LD B, E
+      0x44 => self.ld8(Op8::B, Op8::H),         // LD B, H
+      0x45 => self.ld8(Op8::B, Op8::L),         // LD B, L
+      0x46 => self.ld8(Op8::B, Op8::HLd),       // LD B, (HL)
+      0x47 => self.ld8(Op8::B, Op8::A),         // LD B, A
+      0x48 => self.ld8(Op8::C, Op8::B),         // LD C, B
+      0x49 => self.ld8(Op8::C, Op8::C),         // LD C, C
+      0x4A => self.ld8(Op8::C, Op8::D),         // LD C, D
+      0x4B => self.ld8(Op8::C, Op8::E),         // LD C, E
+      0x4C => self.ld8(Op8::C, Op8::H),         // LD C, H
+      0x4D => self.ld8(Op8::C, Op8::L),         // LD C, L
+      0x4E => self.ld8(Op8::C, Op8::HLd),       // LD C, (HL)
+      0x4F => self.ld8(Op8::C, Op8::A),         // LD C, A
 
-      0x50 => self.unimplemented_inst(),
-      0x51 => self.unimplemented_inst(),
-      0x52 => self.unimplemented_inst(),
-      0x53 => self.unimplemented_inst(),
-      0x54 => self.unimplemented_inst(),
-      0x55 => self.unimplemented_inst(),
-      0x56 => self.unimplemented_inst(),
-      0x57 => self.unimplemented_inst(),
-      0x58 => self.unimplemented_inst(),
-      0x59 => self.unimplemented_inst(),
-      0x5A => self.unimplemented_inst(),
-      0x5B => self.unimplemented_inst(),
-      0x5C => self.unimplemented_inst(),
-      0x5D => self.unimplemented_inst(),
-      0x5E => self.unimplemented_inst(),
-      0x5F => self.unimplemented_inst(),
-      0x60 => self.unimplemented_inst(),
-      0x61 => self.unimplemented_inst(),
-      0x62 => self.unimplemented_inst(),
-      0x63 => self.unimplemented_inst(),
-      0x64 => self.unimplemented_inst(),
-      0x65 => self.unimplemented_inst(),
-      0x66 => self.unimplemented_inst(),
-      0x67 => self.unimplemented_inst(),
-      0x68 => self.unimplemented_inst(),
-      0x69 => self.unimplemented_inst(),
-      0x6A => self.unimplemented_inst(),
-      0x6B => self.unimplemented_inst(),
-      0x6C => self.unimplemented_inst(),
-      0x6D => self.unimplemented_inst(),
-      0x6E => self.unimplemented_inst(),
-      0x6F => self.unimplemented_inst(),
-      0x70 => self.unimplemented_inst(),
-      0x71 => self.unimplemented_inst(),
-      0x72 => self.unimplemented_inst(),
-      0x73 => self.unimplemented_inst(),
-      0x74 => self.unimplemented_inst(),
-      0x75 => self.unimplemented_inst(),
-      0x76 => self.unimplemented_inst(),
-      0x77 => self.unimplemented_inst(),
-      0x78 => self.unimplemented_inst(),
-      0x79 => self.unimplemented_inst(),
-      0x7A => self.unimplemented_inst(),
-      0x7B => self.unimplemented_inst(),
-      0x7C => self.unimplemented_inst(),
-      0x7D => self.unimplemented_inst(),
-      0x7E => self.unimplemented_inst(),
-      0x7F => self.unimplemented_inst(),
-      0x80 => self.unimplemented_inst(),
-      0x81 => self.unimplemented_inst(),
-      0x82 => self.unimplemented_inst(),
-      0x83 => self.unimplemented_inst(),
-      0x84 => self.unimplemented_inst(),
-      0x85 => self.unimplemented_inst(),
-      0x86 => self.unimplemented_inst(),
-      0x87 => self.unimplemented_inst(),
-      0x88 => self.unimplemented_inst(),
-      0x89 => self.unimplemented_inst(),
-      0x8A => self.unimplemented_inst(),
-      0x8B => self.unimplemented_inst(),
-      0x8C => self.unimplemented_inst(),
-      0x8D => self.unimplemented_inst(),
-      0x8E => self.unimplemented_inst(),
-      0x8F => self.unimplemented_inst(),
-      0x90 => self.unimplemented_inst(),
-      0x91 => self.unimplemented_inst(),
-      0x92 => self.unimplemented_inst(),
-      0x93 => self.unimplemented_inst(),
-      0x94 => self.unimplemented_inst(),
-      0x95 => self.unimplemented_inst(),
-      0x96 => self.unimplemented_inst(),
-      0x97 => self.unimplemented_inst(),
-      0x98 => self.unimplemented_inst(),
-      0x99 => self.unimplemented_inst(),
-      0x9A => self.unimplemented_inst(),
-      0x9B => self.unimplemented_inst(),
-      0x9C => self.unimplemented_inst(),
-      0x9D => self.unimplemented_inst(),
-      0x9E => self.unimplemented_inst(),
-      0x9F => self.unimplemented_inst(),
+      0x50 => self.ld8(Op8::D, Op8::B),         // LD D, B
+      0x51 => self.ld8(Op8::D, Op8::C),         // LD D, C
+      0x52 => self.ld8(Op8::D, Op8::D),         // LD D, D
+      0x53 => self.ld8(Op8::D, Op8::E),         // LD D, E
+      0x54 => self.ld8(Op8::D, Op8::H),         // LD D, H
+      0x55 => self.ld8(Op8::D, Op8::L),         // LD D, L
+      0x56 => self.ld8(Op8::D, Op8::HLd),       // LD D, (HL)
+      0x57 => self.ld8(Op8::D, Op8::A),         // LD D, A
+      0x58 => self.ld8(Op8::E, Op8::B),         // LD E, B
+      0x59 => self.ld8(Op8::E, Op8::C),         // LD E, C
+      0x5A => self.ld8(Op8::E, Op8::D),         // LD E, D
+      0x5B => self.ld8(Op8::E, Op8::E),         // LD E, E
+      0x5C => self.ld8(Op8::E, Op8::H),         // LD E, H
+      0x5D => self.ld8(Op8::E, Op8::L),         // LD E, L
+      0x5E => self.ld8(Op8::E, Op8::HLd),       // LD E, (HL)
+      0x5F => self.ld8(Op8::E, Op8::A),         // LD E, A
+
+      0x60 => self.ld8(Op8::H, Op8::B),         // LD H, B
+      0x61 => self.ld8(Op8::H, Op8::C),         // LD H, C
+      0x62 => self.ld8(Op8::H, Op8::D),         // LD H, D
+      0x63 => self.ld8(Op8::H, Op8::E),         // LD H, E
+      0x64 => self.ld8(Op8::H, Op8::H),         // LD H, H
+      0x65 => self.ld8(Op8::H, Op8::L),         // LD H, L
+      0x66 => self.ld8(Op8::H, Op8::HLd),       // LD H, (HL)
+      0x67 => self.ld8(Op8::H, Op8::A),         // LD H, A
+      0x68 => self.ld8(Op8::L, Op8::B),         // LD L, B
+      0x69 => self.ld8(Op8::L, Op8::C),         // LD L, C
+      0x6A => self.ld8(Op8::L, Op8::D),         // LD L, D
+      0x6B => self.ld8(Op8::L, Op8::E),         // LD L, E
+      0x6C => self.ld8(Op8::L, Op8::H),         // LD L, H
+      0x6D => self.ld8(Op8::L, Op8::L),         // LD L, L
+      0x6E => self.ld8(Op8::L, Op8::HLd),       // LD L, (HL)
+      0x6F => self.ld8(Op8::L, Op8::A),         // LD L, A
+
+      0x70 => self.ld8(Op8::HLd, Op8::B),       // LD H, B
+      0x71 => self.ld8(Op8::HLd, Op8::C),       // LD H, C
+      0x72 => self.ld8(Op8::HLd, Op8::D),       // LD H, D
+      0x73 => self.ld8(Op8::HLd, Op8::E),       // LD H, E
+      0x74 => self.ld8(Op8::HLd, Op8::H),       // LD H, H
+      0x75 => self.ld8(Op8::HLd, Op8::L),       // LD H, L
+      0x76 => { self.halt(); return false }     // HALT
+      0x77 => self.ld8(Op8::HLd, Op8::A),       // LD H, A
+      0x78 => self.ld8(Op8::A, Op8::B),         // LD A, B
+      0x79 => self.ld8(Op8::A, Op8::C),         // LD A, C
+      0x7A => self.ld8(Op8::A, Op8::D),         // LD A, D
+      0x7B => self.ld8(Op8::A, Op8::E),         // LD A, E
+      0x7C => self.ld8(Op8::A, Op8::H),         // LD A, H
+      0x7D => self.ld8(Op8::A, Op8::L),         // LD A, L
+      0x7E => self.ld8(Op8::A, Op8::HLd),       // LD A, (HL)
+      0x7F => self.ld8(Op8::A, Op8::A),         // LD A, A
+
+      0x80 => self.add8(Op8::B),                // ADD A, B
+      0x81 => self.add8(Op8::C),                // ADD A, B
+      0x82 => self.add8(Op8::D),                // ADD A, B
+      0x83 => self.add8(Op8::E),                // ADD A, B
+      0x84 => self.add8(Op8::H),                // ADD A, B
+      0x85 => self.add8(Op8::L),                // ADD A, B
+      0x86 => self.add8(Op8::HLd),              // ADD A, B
+      0x87 => self.add8(Op8::A),                // ADD A, B
+      0x88 => self.adc8(Op8::B),                // ADC A, B
+      0x89 => self.adc8(Op8::C),                // ADC A, B
+      0x8A => self.adc8(Op8::D),                // ADC A, B
+      0x8B => self.adc8(Op8::E),                // ADC A, B
+      0x8C => self.adc8(Op8::H),                // ADC A, B
+      0x8D => self.adc8(Op8::L),                // ADC A, B
+      0x8E => self.adc8(Op8::HLd),              // ADC A, B
+      0x8F => self.adc8(Op8::A),                // ADC A, B
+
+      0x90 => self.sub8(Op8::B),                // SUB B
+      0x91 => self.sub8(Op8::C),                // SUB C
+      0x92 => self.sub8(Op8::D),                // SUB D
+      0x93 => self.sub8(Op8::E),                // SUB E
+      0x94 => self.sub8(Op8::H),                // SUB H
+      0x95 => self.sub8(Op8::L),                // SUB L
+      0x96 => self.sub8(Op8::HLd),              // SUB (HL)
+      0x97 => self.sub8(Op8::A),                // SBC A
+      0x98 => self.sbc8(Op8::B),                // SBC B
+      0x99 => self.sbc8(Op8::C),                // SBC C
+      0x9A => self.sbc8(Op8::D),                // SBC D
+      0x9B => self.sbc8(Op8::E),                // SBC E
+      0x9C => self.sbc8(Op8::H),                // SBC H
+      0x9D => self.sbc8(Op8::L),                // SBC L
+      0x9E => self.sbc8(Op8::HLd),              // SBC (HL)
+      0x9F => self.sbc8(Op8::A),                // SBC A
+
       0xA0 => self.unimplemented_inst(),
       0xA1 => self.unimplemented_inst(),
       0xA2 => self.unimplemented_inst(),
@@ -560,6 +634,7 @@ impl State {
       0xAD => self.unimplemented_inst(),
       0xAE => self.unimplemented_inst(),
       0xAF => self.unimplemented_inst(),
+
       0xB0 => self.unimplemented_inst(),
       0xB1 => self.unimplemented_inst(),
       0xB2 => self.unimplemented_inst(),
@@ -576,6 +651,7 @@ impl State {
       0xBD => self.unimplemented_inst(),
       0xBE => self.unimplemented_inst(),
       0xBF => self.unimplemented_inst(),
+
       0xC0 => self.unimplemented_inst(),
       0xC1 => self.unimplemented_inst(),
       0xC2 => self.unimplemented_inst(),
@@ -592,6 +668,7 @@ impl State {
       0xCD => self.unimplemented_inst(),
       0xCE => self.unimplemented_inst(),
       0xCF => self.unimplemented_inst(),
+
       0xD0 => self.unimplemented_inst(),
       0xD1 => self.unimplemented_inst(),
       0xD2 => self.unimplemented_inst(),
@@ -608,6 +685,7 @@ impl State {
       0xDD => self.unimplemented_inst(),
       0xDE => self.unimplemented_inst(),
       0xDF => self.unimplemented_inst(),
+
       0xE0 => self.unimplemented_inst(),
       0xE1 => self.unimplemented_inst(),
       0xE2 => self.unimplemented_inst(),
@@ -619,11 +697,12 @@ impl State {
       0xE8 => self.unimplemented_inst(),
       0xE9 => self.unimplemented_inst(),
       0xEA => self.unimplemented_inst(),
-      0xEB => self.unimplemented_inst(),
+      0xEB => self.ex_de_hl(),                  // EX DE, HL
       0xEC => self.unimplemented_inst(),
       0xED => self.unimplemented_inst(),
       0xEE => self.unimplemented_inst(),
       0xEF => self.unimplemented_inst(),
+
       0xF0 => self.unimplemented_inst(),
       0xF1 => self.unimplemented_inst(),
       0xF2 => self.unimplemented_inst(),
@@ -642,5 +721,7 @@ impl State {
       0xFF => self.unimplemented_inst(),
       _ => unreachable!(),
     }
+
+    true
   }
 }
